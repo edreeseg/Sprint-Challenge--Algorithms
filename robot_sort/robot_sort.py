199f07ddd80e345ea4e811a1deb258864ed0d4be
edreeseg <edward.reeseg@gmail.com>
2019-08-31 13:41:19 -0400

    Completion of MVP - Robot with Bubble Sort

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -96,9 +96,23 @@ class SortingRobot:
         """
         Sort the robot's list.
         """
-        # Fill this out
-        pass
-
+        while not self.light_is_on():
+            self.set_light_on()
+            while self.can_move_right():
+                self.swap_item() # Drop the None value, should always start holding the None
+                self.move_right()
+                if self.compare_item() == 1:
+                    self.set_light_off()
+                    self.swap_item()
+                    self.move_left()
+                    self.swap_item()
+                    self.move_right()
+                else:
+                    self.move_left()
+                    self.swap_item()
+                    self.move_right()
+            while self.can_move_left():
+                self.move_left()
 
 if __name__ == "__main__":
     # Test our your implementation from the command line
